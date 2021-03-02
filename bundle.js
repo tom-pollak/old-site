24d1b57b8d63fcb817bd3d6b895da7f42a9aef25
Tom Pollak <tompollak1000@gmail.com>
2021-03-02 03:09:11 +0000

    update

--- bundle.js
+++ bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\n// Module\nexports.push([module.i, \"p {\\n  color: cyan; }\\n\", \"\"]);\n// Exports\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/sass/styles.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js??ref--4-2");
+eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\n// Module\nexports.push([module.i, \"p {\\n  color: #ff5500; }\\n\", \"\"]);\n// Exports\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/sass/styles.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js??ref--4-2");
 
 /***/ }),
 
@@ -121,6 +121,40 @@ eval("\n\nvar isOldIE = function isOldIE() {\n  var memo;\n  return function mem
 
 /***/ }),
 
+/***/ "./src/js/reload.js":
+/*!**************************!*\
+  !*** ./src/js/reload.js ***!
+  \**************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sass.js */ \"./src/js/sass.js\");\n/* harmony import */ var _sass_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_sass_js__WEBPACK_IMPORTED_MODULE_0__);\n// import Worker from \"./js/sass.worker.js\";\n\n\nvar sass = new _sass_js__WEBPACK_IMPORTED_MODULE_0___default.a(\"./src/js/sass.worker.js\");\nconsole.log(sass);\n\nsass.preloadFiles(\n  \"../sass/\",\n  \"\",\n  [\"styles.scss\", \"colors.scss\", \"hue.scss\"],\n  function callback() {}\n);\n\nconst rangeSlider = document.getElementById(\"color-picker\");\n// rangeSlider.addEventListener(\"mousemove\", handleUpdate);\nrangeSlider.addEventListener(\"change\", handleUpdate);\n\nfunction handleUpdate() {\n  var scss = \"$hue: \" + this.value + \";\";\n  // sass.readFile(\"colors.scss\", function callback(content) {\n  //   scss += content;\n  // });\n  // console.log(scss);\n  // sass.readFile(\"styles.scss\", function callback(content) {\n  //   console.log(content);\n  //   scss += content;\n  // });\n\n  sass.writeFile(\"hue.scss\", scss);\n  sass.readFile(\"hue.scss\", function callback(content) {\n    console.log(content);\n  });\n  sass.readFile(\"styles.scss\", function callback(content) {\n    console.log(content);\n  });\n  sass.compileFile(\"styles.scss\", function callback(result) {\n    console.log(result.text);\n    sass.writeFile(\"styles.scss\", result.text);\n  });\n}\n\n\n//# sourceURL=webpack:///./src/js/reload.js?");
+
+/***/ }),
+
+/***/ "./src/js/sass.js":
+/*!************************!*\
+  !*** ./src/js/sass.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("/* WEBPACK VAR INJECTION */(function(__dirname) {var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*! sass.js - v0.11.1 (f286436) - built 2019-10-20\n  providing libsass 3.6.2 (4da7c4bd)\n  via emscripten 1.38.31 (040e49a)\n */\n\n(function (root, factory) {\n  'use strict';\n  if (true) {\n    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory),\n\t\t\t\t__WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ?\n\t\t\t\t(__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__),\n\t\t\t\t__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));\n  } else {}\n}(this, function () {/*global document*/\n// identify the path sass.js is located at in case we're loaded by a simple\n// <script src=\"path/to/sass.js\"></script>\n// this path can be used to identify the location of\n// * sass.worker.js from sass.js\n// * libsass.js.mem from sass.sync.js\n// see https://github.com/medialize/sass.js/pull/32#issuecomment-103142214\n// see https://github.com/medialize/sass.js/issues/33\nvar SASSJS_RELATIVE_PATH = (function() {\n  'use strict';\n\n  // in Node things are rather simple\n  var hasDir = typeof __dirname !== 'undefined';\n  if (hasDir) {\n    return __dirname;\n  }\n\n  // we can only run this test in the browser,\n  // so make sure we actually have a DOM to work with.\n  if (typeof document === 'undefined' || !document.getElementsByTagName) {\n    return null;\n  }\n\n  // http://www.2ality.com/2014/05/current-script.html\n  var currentScript = document.currentScript || (function() {\n    var scripts = document.getElementsByTagName('script');\n    return scripts[scripts.length - 1];\n  })();\n\n  var path = currentScript && currentScript.src;\n  if (!path) {\n    return null;\n  }\n\n  // [worker] make sure we're not running in some concatenated thing\n  if (path.slice(-8) === '/sass.js') {\n    return path.slice(0, -8);\n  }\n\n  // [sync] make sure we're not running in some concatenated thing\n  if (path.slice(-13) === '/sass.sync.js') {\n    return path.slice(0, -13);\n  }\n\n  return null;\n})() || '.';\n\n/*global Worker, SASSJS_RELATIVE_PATH*/\n'use strict';\n\nvar noop = function(){};\nvar slice = [].slice;\n// defined upon first Sass.initialize() call\nvar globalWorkerUrl;\n\nfunction Sass(workerUrl) {\n  if (!workerUrl && !globalWorkerUrl) {\n    /*jshint laxbreak:true */\n    throw new Error(\n      'Sass needs to be initialized with the URL of sass.worker.js - '\n      + 'either via Sass.setWorkerUrl(url) or by new Sass(url)'\n    );\n    /*jshint laxbreak:false */\n  }\n\n  if (!globalWorkerUrl) {\n    globalWorkerUrl = workerUrl;\n  }\n\n  // bind all functions\n  // we're doing this because we used to have a single hard-wired instance that allowed\n  // [].map(Sass.removeFile) and we need to maintain that for now (at least until 1.0.0)\n  for (var key in this) {\n    if (typeof this[key] === 'function') {\n      this[key] = this[key].bind(this);\n    }\n  }\n\n  this._callbacks = {};\n  this._worker = new Worker(workerUrl || globalWorkerUrl);\n  this._worker.addEventListener('message', this._handleWorkerMessage, false);\n}\n\n// allow setting the workerUrl before the first Sass instance is initialized,\n// where registering the global workerUrl would've happened automatically\nSass.setWorkerUrl = function(workerUrl) {\n  globalWorkerUrl = workerUrl;\n};\n\nSass.style = {\n  nested: 0,\n  expanded: 1,\n  compact: 2,\n  compressed: 3\n};\n\nSass.comments = {\n  'none': 0,\n  'default': 1\n};\n\nSass.prototype = {\n  style: Sass.style,\n  comments: Sass.comments,\n\n  destroy: function() {\n    this._worker && this._worker.terminate();\n    this._worker = null;\n    this._callbacks = {};\n    this._importer = null;\n  },\n\n  _handleWorkerMessage: function(event) {\n    if (event.data.command) {\n      this[event.data.command](event.data.args);\n    }\n\n    this._callbacks[event.data.id] && this._callbacks[event.data.id](event.data.result);\n    delete this._callbacks[event.data.id];\n  },\n\n  _dispatch: function(options, callback) {\n    if (!this._worker) {\n      throw new Error('Sass worker has been terminated');\n    }\n\n    options.id = 'cb' + Date.now() + Math.random();\n    this._callbacks[options.id] = callback;\n    this._worker.postMessage(options);\n  },\n\n  _importerInit: function(args) {\n    // importer API done callback pushing results\n    // back to the worker\n    var done = function done(result) {\n      this._worker.postMessage({\n        command: '_importerFinish',\n        args: [result]\n      });\n    }.bind(this);\n\n    try {\n      this._importer(args[0], done);\n    } catch(e) {\n      done({ error: e.message });\n      throw e;\n    }\n  },\n\n  importer: function(importerCallback, callback) {\n    if (typeof importerCallback !== 'function' && importerCallback !== null) {\n      throw new Error('importer callback must either be a function or null');\n    }\n\n    // callback is executed in the main EventLoop\n    this._importer = importerCallback;\n    // tell worker to activate importer callback\n    this._worker.postMessage({\n      command: 'importer',\n      args: [Boolean(importerCallback)]\n    });\n\n    callback && callback();\n  },\n};\n\nvar commands = 'writeFile readFile listFiles removeFile clearFiles lazyFiles preloadFiles options compile compileFile';\ncommands.split(' ').forEach(function(command) {\n  Sass.prototype[command] = function() {\n    var callback = slice.call(arguments, -1)[0];\n    var args = slice.call(arguments, 0, -1);\n    if (typeof callback !== 'function') {\n      args.push(callback);\n      callback = noop;\n    }\n\n    this._dispatch({\n      command: command,\n      args: args\n    }, callback);\n  };\n});\n\n// automatically set the workerUrl in case we're loaded by a simple\n// <script src=\"path/to/sass.js\"></script>\n// see https://github.com/medialize/sass.js/pull/32#issuecomment-103142214\nSass.setWorkerUrl(SASSJS_RELATIVE_PATH + '/sass.worker.js');\nreturn Sass;\n}));\n/* WEBPACK VAR INJECTION */}.call(this, \"/\"))\n\n//# sourceURL=webpack:///./src/js/sass.js?");
+
+/***/ }),
+
+/***/ "./src/js/theme.js":
+/*!*************************!*\
+  !*** ./src/js/theme.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function getHue() {\n  try {\n    var rangeSlider = document.getElementById(\"color-picker\");\n    return rangeSlider.value;\n  } catch {\n    return 180;\n  }\n}\nmodule.exports = {\n  // hue: rangeSlider.value,\n  hue: getHue(),\n};\n\n\n//# sourceURL=webpack:///./src/js/theme.js?");
+
+/***/ }),
+
 /***/ "./src/main.js":
 /*!*********************!*\
   !*** ./src/main.js ***!
@@ -129,18 +163,7 @@ eval("\n\nvar isOldIE = function isOldIE() {\n  var memo;\n  return function mem
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sass/styles.scss */ \"./src/sass/styles.scss\");\n/* harmony import */ var _sass_styles_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_sass_styles_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _theme_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./theme.js */ \"./src/theme.js\");\n/* harmony import */ var _theme_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_theme_js__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _reload_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./reload.js */ \"./src/reload.js\");\n/* harmony import */ var _reload_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_reload_js__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\n\n//# sourceURL=webpack:///./src/main.js?");
-
-/***/ }),
-
-/***/ "./src/reload.js":
-/*!***********************!*\
-  !*** ./src/reload.js ***!
-  \***********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-eval("// import Sass from \"./sass.worker.js\";\n\nconst rangeSlider = document.getElementById(\"color-picker\");\n// rangeSlider.addEventListener(\"mousemove\", handleUpdate);\nrangeSlider.addEventListener(\"change\", handleUpdate);\n\nfunction handleUpdate() {\n  // var scss = \"$someVar: 123px; p { color: red; }\";\n  // Sass.compile(scss, function callback(result) {\n  //   console.log(result);\n  // });\n  //\n  Sass.writeFile(\"test.scss\", \"$someVar: 123px; p { color: red; }\");\n  Sass.compileFile(\"./styles.scss\", function callback(result) {\n    console.log(result);\n  });\n}\n\n\n//# sourceURL=webpack:///./src/reload.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_theme_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/theme.js */ \"./src/js/theme.js\");\n/* harmony import */ var _js_theme_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_theme_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _js_reload_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/reload.js */ \"./src/js/reload.js\");\n/* harmony import */ var _sass_styles_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./sass/styles.scss */ \"./src/sass/styles.scss\");\n/* harmony import */ var _sass_styles_scss__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_sass_styles_scss__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\n\n\n//# sourceURL=webpack:///./src/main.js?");
 
 /***/ }),
 
@@ -152,17 +175,6 @@ eval("// import Sass from \"./sass.worker.js\";\n\nconst rangeSlider = document.
 /***/ (function(module, exports, __webpack_require__) {
 
 eval("var api = __webpack_require__(/*! ../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ \"./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\");\n            var content = __webpack_require__(/*! !../../node_modules/css-loader/dist/cjs.js!../../node_modules/sass-loader/dist/cjs.js??ref--4-2!./styles.scss */ \"./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js?!./src/sass/styles.scss\");\n\n            content = content.__esModule ? content.default : content;\n\n            if (typeof content === 'string') {\n              content = [[module.i, content, '']];\n            }\n\nvar options = {};\n\noptions.insert = \"head\";\noptions.singleton = false;\n\nvar update = api(content, options);\n\n\n\nmodule.exports = content.locals || {};\n\n//# sourceURL=webpack:///./src/sass/styles.scss?");
-
-/***/ }),
-
-/***/ "./src/theme.js":
-/*!**********************!*\
-  !*** ./src/theme.js ***!
-  \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-eval("function getHue() {\n  console.log(\"getHue\");\n\n  try {\n    var rangeSlider = document.getElementById(\"color-picker\");\n    return rangeSlider.value;\n  } catch {\n    return 180;\n  }\n}\nmodule.exports = {\n  // hue: rangeSlider.value,\n  hue: getHue(),\n};\n\n\n//# sourceURL=webpack:///./src/theme.js?");
 
 /***/ })
 
